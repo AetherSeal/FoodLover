1196addba176616d4b663ed9c958d389cac41f7b
Alejandro Castro <alejandro.castro.q@gmail.com>
2019-03-30 17:30:40 -0300

    ES6 and Bootstrap 4 update

--- javascripts/ajaxUtil.js
+++ javascripts/ajaxUtil.js
@@ -1,68 +1,31 @@
-(function(global) {
+(function (global) {
     //global == window -- toma el scope general desde el que fue invocado que es el browser como tal por ello apunta a window
     var ajaxTools = {};
 
-    /*chequea que se pueda hacer un request al server mediante ajax y devuelve el mismo */
-    function makeRequest() {
-        if (window.XMLHttpRequest) {
-            return (new XMLHttpRequest());
-        } else {
-            global.alert("Ajax is not supported");
-            return null;
-        }
-    }
-
-    /*Chequea que el request esta bien y que el server da el ok*/
-    function makeResponse(request, responseHandler) {
-        if (request.readyState == 4 && request.status == 200) {
-            responseHandler(request);
-        }
-    }
-
-    /*Envia un GET request al server*/
-    ajaxTools.sendGetRequest = function(requestUrl, responseHandler) {
-        var request = makeRequest();
-        request.onreadystatechange = function() {
-            makeResponse(request, responseHandler);
-        }
-        request.open("GET", requestUrl, true);
-        request.send(null);
-    }
-
-    /*Envia un POST request al server*/
-    ajaxTools.sendPostRequest = function(requestUrl, responseHandler) {
-        var request = makeRequest();
-        request.onreadystatechange = function() {
-            makeResponse(request, responseHandler);
-        }
-        request.open("POST", requestUrl, true);
-        request.send(null); //content of post
-    }
-
-    const fetchURL = (request, callback, type='text') => {
+    const fetchURL = (request, callback, type = 'text') => {
         fetch(request)
-        .then(response=>{
-            if (!response.ok) {
-                throw response.status
-            }
-            return type=='json'?response.json():response.text()
-        })
-        .then(data=>{
-            callback(data)
-        })
-        .catch(error =>{
-            console.log(`------------>${error}`)
-        })
+            .then(response => {
+                if (!response.ok) {
+                    throw response.status
+                }
+                return type == 'json' ? response.json() : response.text()
+            })
+            .then(data => {
+                callback(data)
+            })
+            .catch(error => {
+                console.log(`------------>${error}`)
+            })
     }
 
-    ajaxTools.fetchGet = (url, callback,type) => {
+    ajaxTools.fetchGet = (url, callback, type) => {
         fetchURL(url, callback, type)
     }
 
-    ajaxTools.fetchPost = (url, data, callback,type) => {
+    ajaxTools.fetchPost = (url, data, callback, type) => {
         let config = {
-            method:'POST',
-            body:data,
+            method: 'POST',
+            body: data,
             headers: new Headers()
         }
         let request = new request(url, config)
@@ -73,3 +36,7 @@
     global.$ajaxTools = ajaxTools;
 
 })(window);
+
+/****************************************************
+ *https://www.themealdb.com/api.php
+ ****************************************************/
--- javascripts/script.js
+++ javascripts/script.js
@@ -1,136 +1,141 @@
-/*jquery functions*/
-$(function() {
-
-    $("#header-nav button.navbar-toggle").blur(function(argument) {
-        screenSize = window.innerWidth;
-        if (screenSize < 768) {
-            $("#collapsable-nav").collapse('hide');
-        }
-    })
-
-});
-
 /*FoodLover javascript*/
-(function(global) {
-
-    var foodLover = {};
-
-    /*inserta html en un target*/
-    var insertHTML = function(target, html) {
-        var target = document.querySelector(target);
-        target.innerHTML = html;
-    }
-
-    /*agrega html a un elemento*/
-    var addHTML = function(target, html) {
-        var target = document.querySelector(target);
-        target.innerHTML += html;
-    }
-
-    /*inserta el loader de css*/
-    var insertLoader = function(target) {
-        var html = "<div class='loader'></div>";
-        insertHTML(target, html);
-    }
-
-    /*busca y modifica partes en un texto*/
-    var searchAndReplace = function(content, findThis, replaceWith) {
+(function (global) {
+    const foodLover = {};
+    /*
+     * ES6
+     ****************************/
+    /*busca y remplaza expresiones en un string*/
+    const searchAndReplace = (content, findThis, replaceWith) => {
         findThis = new RegExp(findThis, "g");
-        var x = content.replace(findThis, replaceWith);
-
-        return x;
+        return (content.replace(findThis, replaceWith));
     }
-
-    /*carga las categorias del menu para luego ir al detalle*/
-    var insertCategories = function() {
-
-        insertLoader("#mainContent");
-        /*cargamos el container*/
-        $ajaxTools.fetchGet(
-            "snippets/menuItemContainer.html",
-            function(request) {
-                insertHTML("#mainContent", request);
-            },
-        );
-        /*cargamos los items del menu de categorias*/
+    /*inserta un string como innerHTML del target*/
+    const insertHTML = (target, html) => {
+        document.querySelector(target).innerHTML = html;
+    }
+    /*inserta el loader de css*/
+    const insertLoader = target => {
+        $ajaxTools.fetchGet("snippets/loader.html", (html) => {
+            insertHTML(target, html);
+        })
+    }
+    /*carga los items del menu*/
+    const fillMenu = () => {
         $ajaxTools.fetchGet(
             "snippets/menuItem.html",
-            function(request) {
-                const {categories} = foodLover.menuInfo
-                for (var i = categories.length - 1; i >= 0; i--) {
-                    var myRequest = request;
-                    myRequest = searchAndReplace(myRequest, '{{name}}', categories[i].name);
-                    myRequest = searchAndReplace(myRequest, '{{imgPath}}', categories[i].imgPath);
-                    myRequest = searchAndReplace(myRequest, '{{description}}', categories[i].description);
-                    addHTML("#mainContent section.row", myRequest);
-                }
+            (request) => {
+                const {
+                    categories
+                } = foodLover.menuInfo
+                const container = document.querySelector("#mainContent section.row")
+                categories.map(category => {
+                    let categoryTemplate = request;
+                    categoryTemplate = searchAndReplace(categoryTemplate, '{{key}}', category.strCategory);
+                    categoryTemplate = searchAndReplace(categoryTemplate, '{{name}}', category.strCategory);
+                    categoryTemplate = searchAndReplace(categoryTemplate, '{{imgPath}}', category.strCategoryThumb);
+                    categoryTemplate = searchAndReplace(categoryTemplate, '{{description}}', '"' + category.strCategoryDescription + '"');
+                    container.innerHTML += categoryTemplate
+                })
+                document.querySelectorAll("#mainContent .itemTile")
+                    .forEach(element => {
+                        element.addEventListener('click', e => {
+                            fillMenuItems(e.currentTarget.getAttribute('key'))
+                        })
+                    })
             }
         );
     }
-
-    /*revisa los elementos del menu y remueve la clase active*/
-    var clearNavItems = function() {
-        var targets = document.querySelectorAll("#nav-list > li ");
-        for (var i = targets.length - 1; i >= 0; i--) {
-        	var target = document.getElementById(targets[i].id).className;
-            if (target != "" && target != null && target != undefined) {
-            	searchAndReplace(target, "active", "");
-            }
+    const fillMenuItems = category => {
+        console.log('call to category  e.currentTarget.getAttribute("key"):' + category)
+        /*cargamos informacion del restaurant en nuestro scope*/
+        $ajaxTools.fetchGet('https://www.themealdb.com/api/json/v1/1/filter.php?c=' + category, request => {
+                foodLover.menuMeals = request;
+                $ajaxTools.fetchGet(
+                    "snippets/dishes.html",
+                    (request) => {
+                        const {
+                            meals
+                        } = foodLover.menuMeals
+                        const container = document.querySelector("#mainContent section.row")
+                        container.innerHTML = ''
+                        meals.map(meal => {
+                            let mealTemplate = request;
+                            mealTemplate = searchAndReplace(mealTemplate, '{{key}}', meal.idMeal);
+                            mealTemplate = searchAndReplace(mealTemplate, '{{name}}', meal.strMeal);
+                            mealTemplate = searchAndReplace(mealTemplate, '{{imgPath}}', meal.strMealThumb);
+                            container.innerHTML += mealTemplate
+                        })
+                        document.querySelectorAll("#mainContent .dishWrapper")
+                            .forEach(element => {
+                                element.addEventListener('click', e => {
+                                    console.log(e.currentTarget.getAttribute('key'))
+                                    //fillMenuItems(e.currentTarget.getAttribute('key'))
+                                })
+                            })
+                    }
+                );
+            },
+            "json")
+    }
+    /*carga el contenido principal de la pagina*/
+    const fillMainContent = request => {
+        insertHTML("#mainContent", request);
+        document.querySelectorAll('ul>li>a.nav-link').forEach(element => {
+            element.addEventListener('click', menuHandler)
+        })
+    }
+    /*maneja los eventos del menu y las redirecciones*/
+    const menuHandler = e => {
+        /*cambiamos el active element*/
+        let old = document.querySelector('ul>li.nav-item.active')
+        old.classList.remove('active')
+        e.currentTarget.parentElement.classList.add('active')
+        
+        /*cargamos el loader*/
+        insertLoader("#mainContent");
+        const {
+            id
+        } = e.currentTarget
+        switch (id) {
+            case 'navItemMenu':
+                console.log(id)
+                $ajaxTools.fetchGet('./menu.html', request => {
+                    insertHTML("#mainContent", request);
+                    fillMenu()
+                })
+                break;
+            case 'navItemAbout':
+                console.log(id)
+                $ajaxTools.fetchGet('./aboutUs.html', request => {
+                    insertHTML("#mainContent", request);
+                })
+                break;
+            case 'navItemReservation':
+                console.log(id)
+                $ajaxTools.fetchGet('./reservation.html', request => {
+                    insertHTML("#mainContent", request);
+                })
+                break;
+            case 'navItemCall':
+                console.log(id)
+                break;
+            default:
+                break;
         }
     }
-
-    /*coloca como active un elemento del menu */
-    var selectNavItems = function(target){
-    	var targetElem = document.getElementById(target);
-    	targetElem.className+= "active";
-    }
-
-    /*reordena los items del menu de navegacion*/
-    var navBarItems = function (target) {
-    	 clearNavItems();
-    	 selectNavItems(target);
-    	 console.log("we fix the nav bar !!!")
-    }
-
-
+    /***************************/
 
     /*Despues de ser cargado el html pero antes de que se carguen las imagenes y el css*/
-    document.addEventListener("DOMContentLoaded", function(event) {
-
-        /*cargamos informacion del restaurant*/
-        $ajaxTools.fetchGet ("snippets/info.json", request => {
-            debugger
-            var menuInfo = request;
-            foodLover.menuInfo = menuInfo;
-        },'json')
-
+    document.addEventListener("DOMContentLoaded", () => {
+        /*cargamos informacion del restaurant en nuestro scope*/
+        $ajaxTools.fetchGet('https://www.themealdb.com/api/json/v1/1/categories.php', request => {
+                foodLover.menuInfo = request;
+            },
+            "json")
+        /*insertamos un loader para mostrar algo mientras se resuelve la peticion*/
         insertLoader("#mainContent");
-
-
-
-        const fillMainContent = function(request) {
-            
-            insertHTML("#mainContent", request);
-
-            document.querySelector("#mainJumbotron").addEventListener("click", function(event) {
-                console.log("they click the jumbotron");
-            });
-
-            document.querySelector("#menuTile").addEventListener("click", function(event) {
-                navBarItems("navButtonMenu");
-                insertCategories();
-            });
-
-            document.querySelector("#menuMobile").addEventListener("click", function(event) {
-                navBarItems("navButtonMenu");
-                insertCategories();
-            });
-        }
-        
+        /*llenamos el contenido principal de la pagina dentro del template*/
         $ajaxTools.fetchGet("snippets/mainContent.html", fillMainContent)
-
-
-
     });
 
 })(window);
